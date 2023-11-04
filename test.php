34c141851f5a8c16dbd053e016e9a02272068a34
dkozTA <anhthe45776@gmail.com>
2023-11-04 20:51:45 +0700

    add a test php file

---
+++ test.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=, intiial-scale=1.0">
+        
+        <title>
+            fastfood
+        </title>
+    </head>
+
+    <body>
+        <?php
+            $food = "pizza";
+            echo "i like ", $food, "<br>";
+            echo " Date is s: ", date('j-m-y, h');
+        ?>
+    </body>
+</html>
